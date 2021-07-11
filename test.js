98397b0f83d55e3b7a90dc8f745e761a249478e9
jmirin <jmirin@protonmail.com>
2021-07-11 17:01:59 +1000

    test quotesell added

--- test.js
+++ test.js
@@ -10,6 +10,7 @@ var client = new coinspot(key, secret);
 client.status_ro()
 client.status_fa()
 client.quotebuy('HBAR', 'AUD', 6)
+client.quotesell('DOGE', 'coin', 30)
 //var rate = client.quotesell('HBAR', 'coin', 10)
 //console.log(rate)
 // client.instant_sell_now('HBAR', 'coin', 6)
